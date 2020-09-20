e90859971187130245e16f036e900cb3cfa4c43f
Nathan Riviere <n.e.riviere@gmail.com>
2020-09-20 22:36:16 +0200

    Added Layout and AppBar

---
+++ src/components/Layout.js
@@ -0,0 +1,28 @@
+import { Container } from "@material-ui/core";
+import React, { Fragment } from "react";
+import Footer from "./Footer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+export default function Layout({ children }) {
+  return (
+    <Fragment>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">News</Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Container>{children}</Container>
+      <Footer />
+    </Fragment>
+  );
+}
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -6,10 +6,11 @@ import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import Layout from "./Layout";
 
 export default function Questions() {
   return (
-    <Container>
+    <Layout>
       <Typography variant="h2">Questions</Typography>
       <Grid container>
         <Grid item md={3}>
@@ -24,6 +25,6 @@ export default function Questions() {
           </Card>
         </Grid>
       </Grid>
-    </Container>
+    </Layout>
   );
 }
