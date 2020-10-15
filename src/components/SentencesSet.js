8baf6ec26ac9ac39520b33f5d0ce3278c456e6bb
Nathan Riviere <n.e.riviere@gmail.com>
2020-10-15 08:13:31 +0200

    Added shuffle for sentences

--- src/components/SentencesSet.js
+++ src/components/SentencesSet.js
@@ -2,13 +2,38 @@ import React, { useEffect, useState } from "react";
 import { getSentencesSet } from "../services/sentencesSetsService";
 import Layout from "./Layout";
 
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
 export default function SentencesSet({ match }) {
     const [sentences, setSentence] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
         async function fetchData() {
-            await setSentence(getSentencesSet(match.params.id));
+            const s = getSentencesSet(match.params.id);
+            const s_array = [];
+
+            s.sentences.forEach((s_) => s_array.push(shuffle(s_.split(" "))));
+            console.log(s_array);
+            await setSentence(s_array);
             setIsLoaded(true);
         }
 
@@ -25,8 +50,8 @@ export default function SentencesSet({ match }) {
         <Layout>
             <h1>Hi</h1>
 
-            {sentences.sentences.map((s) => (
-                <p key={s}>{s}</p>
+            {sentences.map((s) => (
+                <p key={s.join(" ")}>{s.join(" ")}</p>
             ))}
         </Layout>
     );
